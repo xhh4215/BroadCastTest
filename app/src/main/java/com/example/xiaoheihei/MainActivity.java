dc1cfbca854dac7dc01736e17031896d885e4f78
xhh_git <18734184215@163.com>
2017-10-09 09:55:00 +0800

    通过广播监测系统的网络的变化

--- app/src/main/java/com/example/xiaoheihei/MainActivity.java
+++ app/src/main/java/com/example/xiaoheihei/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.xiaoheihei;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private IntentFilter intentFilter;
+    private MyReceiver myReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        //网络状态发生变化的时候系统会发送的一条广播
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        myReceiver = new MyReceiver();
+        registerReceiver(myReceiver,intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(myReceiver);
+    }
+    class MyReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO: This method is called when the BroadcastReceiver is receiving
+            // an Intent broadcast.
+            ConnectivityManager connectionManager =(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+            if(networkInfo!=null&&networkInfo.isAvailable()){
+                Toast.makeText(context, "net work is availabel", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context, "net work is not availabel", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
     }
 }
